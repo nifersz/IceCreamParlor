554d75a0b94bc27d83756e67c0eaf6983557fe46
Nelson Fernandez <nifersz@icloud.com>
2020-09-26 15:24:25 -0500

    Create project using Android Studio IDE(with base for CLEAN architecture)

---
+++ presentation/src/main/java/icecream/parlor/presentation/ui/main/ActivityMain.kt
@@ -0,0 +1,12 @@
+package icecream.parlor.presentation.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import icecream.parlor.R
+
+class ActivityMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
